06530b8d2178a649402d8514d08f5b45bfac1b93
kimookpong <kimookpong@gmail.com>
2021-03-12 11:20:58 +0700

    v1
    v,dslf,d

---
+++ cfg/index.php
@@ -0,0 +1,97 @@
+
+<!doctype html>
+<html lang="en" dir="ltr">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+<link rel="icon" href="favicon.ico" type="image/x-icon"/>
+
+<title>:: WU HRMS :: Login</title>
+
+<!-- Bootstrap Core and vandor -->
+<link rel="stylesheet" href="../assets/plugins/bootstrap/css/bootstrap.min.css" />
+
+<!-- Core css -->
+<link rel="stylesheet" href="../assets/css/main.css"/>
+<link rel="stylesheet" href="../assets/css/theme1.css"/>
+
+</head>
+<body class="font-montserrat sidebar_dark">
+
+<div class="auth">
+    <div class="auth_left">
+        <div class="card">
+            <div class="text-center mb-2">
+               
+                                <div class="row">
+                <div class="col-md-4"> </div>
+                   <div class="selectgroup w-100 col-8">
+									   <label class="selectgroup-item">
+                                            <input type="radio" name="value" value="TH" class="selectgroup-input" checked="">
+                                            <span class="selectgroup-button">ภาษาไทย</span>
+                                        </label>
+                                        <label class="selectgroup-item">
+                                            <input type="radio" name="value" value="ENG" class="selectgroup-input">
+                                            <span class="selectgroup-button">ENGLISH</span>
+                                        </label>
+                                       
+                                 
+                                    </div>
+                </div>
+
+            </div>
+            <div class="card-body">
+                <div class="card-title text-center">WU HRMS<br>Walailak Univesity</div>
+                <div class="form-group">
+                    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="User account">
+                </div>
+                <div class="form-group">
+                    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                </div>
+                <!-- <div class="form-group">
+                    <label class="custom-control custom-checkbox">
+                    <input type="checkbox" class="custom-control-input" />
+                    <span class="custom-control-label">Remember me</span>
+                    </label>
+                </div> -->
+                <div class="form-footer">
+                    <a href="index2.php" class="btn btn-primary btn-block" title="">Sign in</a>
+                </div>
+            </div>
+        </div>        
+    </div>
+    <div class="auth_right">
+        <div class="carousel slide" data-ride="carousel" data-interval="3000">
+            <div class="carousel-inner">
+                <div class="carousel-item active">
+                    <img src="../assets/images/slider2.svg" class="img-fluid" alt="login page" />
+                    <div class="px-4 mt-4">
+                        <h4>WU HRMS</h4>
+                        <p>Human Resource Managenent System : Walailak University</p>
+                    </div>
+                </div>
+                <!-- <div class="carousel-item">
+                    <img src="../assets/images/slider2.svg" class="img-fluid" alt="login page" />
+                    <div class="px-4 mt-4">
+                        <h4>Quality Code and Easy Customizability</h4>
+                        <p>There are many variations of passages of Lorem Ipsum available.</p>
+                    </div>
+                </div>
+                <div class="carousel-item">
+                    <img src="../assets/images/slider3.svg" class="img-fluid" alt="login page" />
+                    <div class="px-4 mt-4">
+                        <h4>Cross Browser Compatibility</h4>
+                        <p>Overview We're a group of women who want to learn JavaScript.</p>
+                    </div>
+                </div>
+            </div> -->
+        </div>
+    </div>
+</div>
+
+<script src="../assets/bundles/lib.vendor.bundle.js"></script>
+<script src="../assets/js/core.js"></script>
+</body>
+</html>
---
+++ tal/menu.php
@@ -0,0 +1,70 @@
+    <div id="left-sidebar" class="sidebar ">
+        <h5 class="brand-name">WU-HRMS <!--<a href="javascript:void(0)" class="menu_option float-right"><i class="icon-grid font-16" data-toggle="tooltip" data-placement="left" title="Grid & List Toggle"></i></a>--></h5>
+        <h6 class="brand-name"><i class="fa fa-id-card"></i> ลงเวลาปฏิบัตืงานและการลา</h6>
+        <nav id="left-sidebar-nav" class="sidebar-nav">
+            <ul class="metismenu">
+                <li class="active">
+                    <a href="javascript:void(0)" class="has-arrow arrow-c" aria-expanded="true"><i class="icon-rocket"></i><span>สำหรับทมอ</span></a>
+                    <ul aria-expanded="true" class="collapse in" style="">
+                        <li class="active"><a href="index.php"><span>หน้าหลัก</span></a></li>
+                        <li><a href="HRMS_admin_holiday.php"><span>ตั้งค่ากำหนดวันหยุดประจำปี</span></a></li>
+                        <li><a href="HRMS_1_14_punish_list.php"><span>ตั้งค่ากำหนดกะเวลา</span></a></li>
+                        <li><a href="HRMS_1_13_admin_personal_req.php"><span>ตั้งค่ากะเวลา+ตำแหน่ง </a></li>
+                        <li><a href="HRMS_admin_req.php"><span>ตั้งค่ากำหนดประเภทการลา</a></li>
+                        <li><a href="HRMS_admin_req.php"><span>กำหนดอัตราค่าตอบแทนพิเศษ OT และเงื่อนไข</a></li>
+                        <li><a href="HRMS_admin_req.php"><span>ตั้งค่ากำหนดจำนวนวันลาและเงื่อนไขของแต่ละประเภทการลาและสัญญาจ้าง</a></li>      
+                        <li><a href="HRMS_admin_req.php"><span>สืบค้นการลาของพนักงาน สำหรับเจ้าหน้าที่ ทมอ</a></li>      
+                        <li><a href="HRMS_admin_req.php"><span>หน้าสำหรับเรียกดูข้อมูลการปฎิบัติงานรายเดือนของพนักงาน สำหรับเจ้าหน้าที่ ทมอ</a></li>      
+                                          
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="has-arrow arrow-c" aria-expanded="true"><i class="icon-rocket"></i><span>สำหรับพนักงาน</span></a>
+                    <ul aria-expanded="false" class="collapse" style="">
+                        <li><a href="index.php"><span>หน้าแรก</span></a></li>
+                        <li><a href="HRMS_1_13_personal_req.php"><span>บันทึกคำร้องขอลา</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>บันทึกคำร้องขอปฏิบัตงานนอกเวลา OT</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>บันทึกการเข้างานโดยใช้ตำแหน่ง GPS</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>สืบค้นข้อมูลการเข้า-ออก ของตนเอง</span></a></li> 
+                        <li><a href="HRMS_user_req.php"><span>บันทึกคำร้องขอเบิกค่าตอบแทนปฏิบัติงานนอกเวลา OT</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>เรียกดูข้อมูลการปฎิบัติงานรายเดือนของตนเอง</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>หน้าอนุมัติคำร้องขอเบิกค่าตอบแทนปฏิบัติงานนอกเวลา OT สำหรับผู้ควบคุมงาน</span></a></li>  
+                    </ul>
+                </li>
+                <li>
+                    <a href="javascript:void(0)" class="has-arrow arrow-c" aria-expanded="true"><i class="icon-rocket"></i><span>หัวหน้าฝ่าย</span></a>
+                    <ul aria-expanded="false" class="collapse" style="">
+                        <li><a href="index.php"><span>หน้าแรก</span></a></li>
+                        <li><a href="HRMS_1_13_personal_req.php"><span>หน้าอนุมัติการลาสำหรับหัวหน้าฝ่าย</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>สืบค้นการลาของผู้ใต้บังคับบัญชา สำหรับหัวหน้าฝ่าย</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>หน้าสำหรับเรียกดูข้อมูลการปฎิบัติงานรายเดือนของพนักงาน สำหรับหัวหน้าฝ่าย</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>หน้าอนุมัติการเข้าออกงานผ่านการใช้ตำแหน่ง GPS</span></a></li>  
+                    </ul>
+                </li>
+				<li>
+                    <a href="javascript:void(0)" class="has-arrow arrow-c" aria-expanded="true"><i class="icon-rocket"></i><span>หัวหน้าหน่วยงาน</span></a>
+                    <ul aria-expanded="false" class="collapse" style="">
+                        <li><a href="HRMS_1_13_personal_req.php"><span>หน้าอนุมัติการลาสำหรับผู้อำนวยการ</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>หน้าอนุมัติคำร้องขอปฏิบัติงานนอกเวลา OT สำหรับหัวหน้าหน่วยงาน</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>หน้าอนุมัติคำร้องขอเบิกค่าตอบแทนปฏิบัติงานนอกเวลา OT สำหรับหัวหน้าหน่วยงาน</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>สืบค้นการลาของผู้ใต้บังคับบัญชา สำหรับหัวหน้าหน่วยงาน</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>หน้าสำหรับเรียกดูข้อมูลการปฎิบัติงานรายเดือนของพนักงาน สำหรับหัวหน้าหน่วยงาน</span></a></li>  
+                       
+                    </ul>
+                </li>
+	<li>
+                    <a href="javascript:void(0)" class="has-arrow arrow-c" aria-expanded="true"><i class="icon-rocket"></i><span>รองอธิการบดี</span></a>
+                    <ul aria-expanded="false" class="collapse" style="">
+                        <li><a href="HRMS_1_13_personal_req.php"><span>หน้าอนุมัติคำร้องขอปฏิบัติงานนอกเวลา OT สำหรับรองในกำกับ</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>หน้าอนุมัติคำร้องขอเบิกค่าตอบแทนปฏิบัติงานนอกเวลา OT สำหรับรองในกำกับ</span></a></li>
+                        <li><a href="HRMS_user_req.php"><span>สืบค้นการลาของผู้ใต้บังคับบัญชา สำหรับรองอธิการบดี</span></a></li>  
+                        <li><a href="HRMS_user_req.php"><span>หน้าสำหรับเรียกดูข้อมูลการปฎิบัติงานรายเดือนของพนักงาน สำหรับหัวหน้ารองอธิการบดี</span></a></li>  
+                       
+                    </ul>
+                </li>                
+                
+                
+                
+            </ul>
+        </nav>          
+    </div>
